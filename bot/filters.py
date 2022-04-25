8e7c9901478ef198862887863301bf9da9a7ccf3
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2022-04-25 15:45:24 +0530

    fixing for pyrogram v2.0.8

--- bot/filters.py
+++ bot/filters.py
@@ -31,68 +31,57 @@ class CustomFilters:
     )
 
     request_filter = (
-        ~filters.edited
-        & filters.text
+        filters.text
         & _main_group_filter
     )
 
     fulfill_filter = (
-        ~filters.edited
-        & _main_group_filter
+        _main_group_filter
         & (filters.audio | filters.voice | filters.document | _ezbook_bot_filter)
     )
 
     start_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("start", COMMAND_PREFIXES)
     )
 
     help_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("help", COMMAND_PREFIXES)
     )
 
     limits_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("limits", COMMAND_PREFIXES)
     )
 
     dropdb_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("dropdb", COMMAND_PREFIXES)
     )
 
     dellastreq_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("dellastreq", COMMAND_PREFIXES)
     )
 
     delreq_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("delreq", COMMAND_PREFIXES)
     )
 
     done_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("done", COMMAND_PREFIXES)
     )
 
     notdone_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("pending", COMMAND_PREFIXES)
     )
 
     requests_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & (
             filters.command("me", COMMAND_PREFIXES) |
             filters.command("userrequests", COMMAND_PREFIXES)
@@ -100,49 +89,41 @@ class CustomFilters:
     )
 
     stats_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("stats", COMMAND_PREFIXES)
     )
 
     pending_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("requests", COMMAND_PREFIXES)
     )
 
     lastfilled_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("lastfilled", COMMAND_PREFIXES)
     )
 
     sqlquery_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command(["sqlquery", "sqlqueryop"], COMMAND_PREFIXES)
     )
 
     schemas_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("schemas", COMMAND_PREFIXES)
     )
 
     formfulfilled_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("formfulfilled", COMMAND_PREFIXES)
     )
 
     leaderboard_cmd_filter = (
-        ~filters.edited
-        & (_sudo_user_filter | _main_group_filter)
+        (_sudo_user_filter | _main_group_filter)
         & filters.command("leaderboard", COMMAND_PREFIXES)
     )
 
     backupdb_cmd_filter = (
-        ~filters.edited
-        & _sudo_user_filter
+        _sudo_user_filter
         & filters.command("backupdb", COMMAND_PREFIXES)
     )
